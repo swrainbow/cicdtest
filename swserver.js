5ab44d621d5ace21f1555294cfa3e1f57d1fc88e
shen wei <534965993@qq.com>
2024-02-03 15:24:30 +0800

    fix1

--- swserver.js
+++ swserver.js
@@ -7,5 +7,5 @@ http.createServer(function (request, response) {
     response.writeHead(200, {'Content-Type': 'text/plain'});
 
     // 发送响应数据 "Hello World"
-    response.end('Hello Worldd 8091\n');
-}).listen(8091);
+    response.end('from load Worldd 8091\n');
+}).listen(8089);
